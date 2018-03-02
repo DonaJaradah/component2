05dc3170fed5c916df940214da39dd0da5f5fb81
NadineAbdelaziz <36333744+NadineAbdelaziz@users.noreply.github.com>
2018-03-02 17:48:21 +0200

    Add files via upload

---
+++ src/components/Dashboard/Views/Store.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
+          <paper-table :title="table1.title" :sub-title="table1.subTitle" :data="table1.data" :columns="table1.columns">
+
+          </paper-table>
+        </div>
+      </div>
+
+    </div>
+</template>
+<script>
+  import PaperTable from 'components/UIComponents/PaperTable.vue'
+  const tableColumns = ['Id', 'Name', 'Price', 'CreatedAt', 'UpdatedAt', 'SellerName']
+  const tableData = [{
+    id: 1,
+    name: 'Backpack',
+    price: '$30',
+    createdat: '15/2/2018',
+    updatedat: '16/2/2018',
+    sellername: 'Nadine Adel'
+  },
+  {
+    id: 2,
+    name: 'Clock',
+    price: '$40',
+    createdat: '11/2/2018',
+    updatedat: '17/4/2018',
+    sellername: 'Dona Jaradah'
+  },
+  {
+    id: 3,
+    name: 'Laptops',
+    price: '$55',
+    createdat: '18/9/2018',
+    updatedat: '16/2/2018',
+    sellername: 'Lina Hany'
+  },
+  {
+    id: 4,
+    name: 'Bottles',
+    price: '$70',
+    createdat: '19/2/2018',
+    updatedat: '13/2/2018',
+    sellername: 'David Hany'
+  },
+  {
+    id: 5,
+    name: 'Clothes',
+    price: '$100',
+    createdat: '15/2/2018',
+    updatedat: '16/2/2018',
+    sellername: 'Menna Wael'
+  }
+]
+
+  export default {
+    components: {
+      PaperTable
+    },
+    data () {
+      return {
+        table1: {
+          title: 'Component 2',
+          subTitle: ' Amazon ',
+          columns: [...tableColumns],
+          data: [...tableData]
+        },
+        table2: {
+          title: 'Table on Plain Background',
+          subTitle: 'Here is a subtitle for this table',
+          columns: [...tableColumns],
+          data: [...tableData]
+        }
+      }
+    }
+  }
+
+</script>
+<style>
+
+</style>
---
+++ src/components/Dashboard/Views/UserProfile/UserCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="card card-user">
+    <div class="image">
+      <img src="static/img/background.jpg" alt="...">
+    </div>
+    <div class="content">
+      <div class="author">
+        <img class="avatar border-blue" src="static\img\liza.jpg" alt="...">
+        <h4 class="title">Nadine Adel
+          <br>
+
+        </h4>
+      </div>
+      <p class="description text-center">
+        "I like the way you work it
+        <br> SE style
+        <br> I love music"
+      </p>
+    </div>
+    <hr>
+
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        details: [
+          {
+            title: '12',
+            subTitle: 'Files'
+          },
+          {
+            title: '2GB',
+            subTitle: 'Used'
+          },
+          {
+            title: '24,6$',
+            subTitle: 'Spent'
+          }
+        ]
+      }
+    },
+    methods: {
+      getClasses (index) {
+        var remainder = index % 3
+        if (remainder === 0) {
+          return 'col-md-3 col-md-offset-1'
+        } else if (remainder === 2) {
+          return 'col-md-4'
+        } else {
+          return 'col-md-3'
+        }
+      }
+    }
+  }
+
+</script>
+<style>
+
+</style>
